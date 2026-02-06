db07f0c0b617412f795977ab02a291d66e134196
Adrian Webb <adrian@webb.sh>
2026-02-06 13:07:50 -0500

    Updating the integration test configuration to use absolute paths.

--- vitest.integration.config.ts
+++ vitest.integration.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths({ projects: [path.resolve(__dirname, 'tsconfig.json')] })],
+  root: __dirname,
   test: {
     fileParallelism: false,
     environment: 'node',
@@ -22,9 +28,9 @@ export default defineConfig({
   resolve: {
     alias: {
       'astro:schema': 'zod',
-      '@tests': './tests',
-      '@modules': './modules',
-      '@': './src',
+      '@tests': path.resolve(__dirname, './tests'),
+      '@modules': path.resolve(__dirname, './modules'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
