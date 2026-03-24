b5ad8cf7f43648a798b48754312e6b249ac4858f
Adrian Webb <adrian@webb.sh>
2026-03-24 02:31:50 -0400

    Fixing issues with the API docs library.

--- src/lib/api/api-docs.ts
+++ src/lib/api/api-docs.ts
@@ -186,7 +186,12 @@ export async function generateDocs(
   // The possible base paths for module APIs
   const basePaths = isCore
     ? ['../../pages/api', '/src/pages/api']
-    : [`/modules/${moduleName}/src/pages/api`];
+    : [
+        `/modules/${moduleName}/src/pages/api`,
+        `../../../modules/${moduleName}/src/pages/api`,
+        `../../../../apps/backend/modules/${moduleName}/src/pages/api`,
+        `../../../../apps/frontend/modules/${moduleName}/src/pages/api`,
+      ];
 
   const allApiModules = GlobHelper.getApiModules();
   for (const [fileIdentifier, modFn] of Object.entries(allApiModules)) {
